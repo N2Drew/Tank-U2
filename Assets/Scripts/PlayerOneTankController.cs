bc00c71846a7e9c9d61b532c1a53a9e8a248fba5
Andrew <andrew.surber@ymail.com>
2017-06-05 16:07:56 +0200

    Initial Merge
    Includes:
    - Some sprites
    - Some controls

---
+++ Assets/Scripts/PlayerOneTankController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerOneTankController : MonoBehaviour {
+
+    Rigidbody2D tankBody;
+    public float moveSpeed = 5;
+    private float turningRadius = 0;
+
+	// Use this for initialization
+	void Start () {
+        tankBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector2 moveVector = new Vector2(CrossPlatformInputManager.GetAxisRaw("PlayerOneHorizontal"), 
+            CrossPlatformInputManager.GetAxisRaw("PlayerOneVertical"));
+        bool isFiring = CrossPlatformInputManager.GetButton("PlayerOneFire");
+
+        //tankBody.AddForce(moveVector);
+        tankBody.velocity = moveVector;
+	}
+}
